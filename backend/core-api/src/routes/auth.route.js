87d0388e57e2c2c8f01bea53a903afaed69b3db0
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2026-01-05 16:01:41 +0530

    [#7160]feat: Add auth routes

---
+++ backend/core-api/src/routes/auth.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import validate from '../middlewares/validate.js';
+import authValidation from '../validations/auth.validation.js';
+import authController from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/register', validate(authValidation.register), authController.register);
+router.post('/login', validate(authValidation.login), authController.login);
+
+export default router;
